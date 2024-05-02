2e3c11d34501c7a0a6c0521d994a9ff728a948fe
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-05-02 16:17:14 -0500

    Exercise 2 LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace LINQ
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<string> gameNames = new List<string>
+            {"Madden", "GTA", "NHL", "Minecraft" };
+
+            var shortToLong = gameNames.OrderByDescending(word => word.Length);
+
+            Console.WriteLine("Words ordered from shortest to longest:");
+            foreach (var word in shortToLong)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
